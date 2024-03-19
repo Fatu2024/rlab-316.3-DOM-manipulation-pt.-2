12db98904b8c567c436d1f426caa336f997a66fb
Fatu2024 <fatumab3@gmail.com>
2024-03-18 21:12:52 -0400

    read me update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rlab-316.3-DOM-manipulation-pt.-2
+first four part of part 2 are done
--- script.js
+++ script.js
@@ -71,42 +71,47 @@ const subMenuEl = document.querySelector('#sub-menu');
 // Set the height subMenuEl element to be "100%".
 subMenuEl.style.height = '100%';
 // Set the background color of subMenuEl to the value stored in the --sub-menu-bg CSS custom property.
-subMenuEl.style.backgroundColor = 'var(--sub-men-bg)';
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
 // Add the class of flex-around to the subMenuEl element.
 subMenuEl.classList.add('flex-around');
 
 // Set the CSS position property of subMenuEl to the value of absolute.
-subMenuEl.style.positiion = 'absolute';
+subMenuEl.style.position = 'absolute';
 
 // Set the CSS top property of subMenuEl to the value of 0.
 subMenuEl.style.top = '0';
 
 // Part 4: Adding Menu Interaction
-// 1. Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
+// 1. Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
 const topMenulinks = topMenuEl.querySelectorAll('a');
-
-// 2. Attach a delegated 'click' event listener to topMenuEl.
+// 2. Attach a delegated 'click' event listener to topMenuEl.
 topMenuEl.addEventListener('click', function (evt){
-  // The first line of code of the event listener function should call the event object's preventDefault() method.
+  // The first line of code of the event listener function should call the event object's preventDefault() method.
 evt.preventDefault();
-// The second line of code of the function should immediately return if the element clicked was not an <a> element.
-if (evt.target === topMenuEl){
-  console.log('not a link')
-} else {
-  console.log(evt.target)
+// The second line of code of the function should immediately return if the element clicked was not an <a> element.
+if (evt.target !== 'A'){
+  console.log('not a link');
+  return;
 }
-// 1. The event listener should add the active class to the <a> element that was clicked, unless it was already active, in which case it should remove it.
+// 1. The event listener should add the active class to the <a> element that was clicked, unless it was already active, in which case it should remove it.
 const clicked = evt.target;
 clicked.classList.toggle('active');
-
-// 2. The event listener should remove the active class from each other <a> element in topMenuLinks - whether the active class exists or not.
-// Hint: Removing a non-existent class from an element does not cause an error!
+// 2. The event listener should remove the active class from each other <a> element in topMenuLinks - whether the active class exists or not.
+// Hint: Removing a non-existent class from an element does not cause an error!
 topMenulinks.forEach(link => {
-  if (link !== evt.target){
+  if (link !== clicked){
     link.classList.remove('active')
   }
 });
+const subMenuLink = menuLinks.find(item => item.text === clicked.innerText);
+if (subMenuLink && subMenuLink.subLinks){
+  subMenuEl.style.top = '100%';
+} else {
+  subMenuEl.style.top = '0';
 }
-)
+});
+// Part 5: Adding Submenu Interaction
+// Within the event listener, if the clicked <a> element does not yet have a class of "active" (it was inactive when clicked):
+// If the clicked <a> element's "link" object within menuLinks has a subLinks property (all do, except for the "link" object for ABOUT), set the CSS top property of subMenuEl to 100%.
 
 
